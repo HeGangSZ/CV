eb4dbb794fb5489420aa55d0530b30199857e81d
HeGangSZ <493910728@qq.com>
2016-03-01 21:49:18 +0800

    完善内容20160301

--- js/resume.js
+++ js/resume.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         anchors:['1', '2','3','4','5','6','7'],
         navigation: true,
         navigationPosition: 'right',
-        navigationTooltips: ['我的简历', '擅长技术', '项目经验',  '基本信息','我的优点','想说的话', '联系我吧'],
+        navigationTooltips: ['我的简历', '擅长技术', '项目经验',  '基本信息','核心竞争力','想说的话', '联系我吧'],
         loopBottom: true,
         afterRender: function(){
 
@@ -94,12 +94,76 @@ $(document).ready(function() {
             if(index ==7){
                 $('.contact').addClass('show');
                 $('.contact').removeClass('hide')
+                $('.phone').addClass('floating')
             }
             else if(index !=7){
                 $('.contact').addClass('hide');
                 $('.contact').removeClass('show');
+                $('.phone').removeClass('floating')
             }
 
         }
     });
+
+    var myChart = echarts.init(document.getElementById('power'));
+
+    option = {
+        backgroundColor: '#e7e7e7',
+        title: {
+            text: '核心竞争力',
+            left: 'center',
+            top:'80px',
+            textStyle: {
+                fontSize:40,
+                color: '#000'
+            }
+        },
+        visualMap: {
+            show: false,
+            min: 80,
+            max: 600,
+            inRange: {
+                colorLightness: [0, 1]
+            }
+        },
+        series : [
+            {
+                //name: '访问来源',
+                type: 'pie',
+                radius: '55%',
+                data:[
+                    {value:250, name:'具备抗压能力，再难的挑战都尽力完成！'},
+                    {value:274, name:'富有责任感，每个任务都尽善尽美。'},
+                    {value:330, name:'饱含技术激情，努力钻研付诸实践！'},
+                    {value:380, name:'快速学习，具备一定的自学能力'},
+                ],
+                roseType: 'angle',
+                label: {
+                    normal: {
+                        textStyle: {
+                            fontSize:20,
+                            color: 'rgba(0, 0, 0, 0.7)'
+                        }
+                    }
+                },
+                labelLine: {
+                    normal: {
+                        lineStyle: {
+                            color: 'rgba(0, 0, 0, 0.3)'
+                        }
+                    }
+                },
+                itemStyle: {
+                    normal: {
+                        color: '#c23531',
+                        shadowBlur: 200,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+
+    myChart.setOption(option);
+
 });
